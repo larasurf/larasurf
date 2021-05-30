fdd723193e08f0937cdd586d7265d2d83939e451
Brice Hartmann <brice@bricehartmann.com>
2021-05-30 13:43:37 -0500

    formatting

--- bin/surf.sh
+++ bin/surf.sh
@@ -6,25 +6,25 @@ function exit_if_containers_not_running() {
   CONTAINERS_RUNNING="$(docker-compose ps -q)"
 
   if [[ -z "$CONTAINERS_RUNNING" ]]; then
-    echo "Containers are not running!"
+    echo 'Containers are not running!'
 
     exit 1
   fi
 }
 
-if [[ "$1" == "ssl" ]]; then
-  if [[ "$2" == "generate" ]]; then
+if [[ "$1" == 'ssl' ]]; then
+  if [[ "$2" == 'generate' ]]; then
     if [[ -n "$(which mkcert.exe)" ]]; then
       mkcert.exe -install && mkcert.exe -key-file .docker/ssl/local.pem -cert-file .docker/ssl/local.crt localhost
     elif [[ -n "$(which mkcert)" ]]; then
       mkcert -install && mkcert -key-file .docker/ssl/local.pem -cert-file .docker/ssl/local.crt localhost
     else
-      echo "To use local SSL, please install mkcert from: https://github.com/FiloSottile/mkcert"
+      echo 'To use local SSL, please install mkcert from: https://github.com/FiloSottile/mkcert'
       exit
     fi
-  elif [[ "$2" == "nginx-config" ]]; then
-    if [[ -f ".docker/nginx/laravel.conf.template" ]] && ! grep -q "listen 443 ssl;" ".docker/nginx/laravel.conf.template"; then
-      cat << 'EOF' >> ".docker/nginx/laravel.conf.template"
+  elif [[ "$2" == 'nginx-config' ]]; then
+    if [[ -f '.docker/nginx/laravel.conf.template' ]] && ! grep -q 'listen 443 ssl;' '.docker/nginx/laravel.conf.template'; then
+      cat << 'EOF' >> '.docker/nginx/laravel.conf.template'
 
 server {
     listen 443 ssl;
@@ -51,51 +51,51 @@ server {
 }
 EOF
 
-      echo "NGINX config template updated."
-      echo "Rebuild the webserver image then restart the container!"
-    elif grep -q "listen 443 ssl;" ".docker/nginx/laravel.conf.template"; then
-      echo "NGINX config template already listens on port 443"
+      echo 'NGINX config template updated.'
+      echo 'Rebuild the webserver image then restart the container!'
+    elif grep -q 'listen 443 ssl;' '.docker/nginx/laravel.conf.template'; then
+      echo 'NGINX config template already listens on port 443'
     else
-      echo "NGINX config template not found"
+      echo 'NGINX config template not found'
     fi
   else
-    echo "Unrecognized ssl command"
+    echo 'Unrecognized ssl command'
   fi
-elif [[ "$1" == "publish" ]]; then
-  if [[ "$2" == "cs-fixer-config" ]]; then
+elif [[ "$1" == 'publish' ]]; then
+  if [[ "$2" == 'cs-fixer-config' ]]; then
     docker-compose run --rm --no-deps laravel php ./vendor/larasurf/larasurf/src/surf.php publish cs-fixer-config
   else
-    echo "Unrecognized publish command"
+    echo 'Unrecognized publish command'
   fi
-elif [[ "$1" == "composer" ]]; then
+elif [[ "$1" == 'composer' ]]; then
   cd $(pwd)
   docker-compose run --rm --no-deps laravel composer "${@:2}"
-elif [[ "$1" == "yarn" ]]; then
+elif [[ "$1" == 'yarn' ]]; then
   cd $(pwd)
   docker-compose run --rm --no-deps laravel yarn "${@:2}"
-elif [[ "$1" == "artisan" ]]; then
+elif [[ "$1" == 'artisan' ]]; then
   exit_if_containers_not_running
 
   cd $(pwd)
   docker-compose exec laravel php artisan "${@:2}"
-elif [[ "$1" == "test" ]]; then
+elif [[ "$1" == 'test' ]]; then
   exit_if_containers_not_running
 
   cd $(pwd)
   docker-compose exec laravel ./vendor/bin/phpunit "${@:2}"
-elif [[ "$1" == "fix" ]]; then
+elif [[ "$1" == 'fix' ]]; then
   exit_if_containers_not_running
 
-  if grep -q "\"barryvdh/laravel-ide-helper\"" "composer.json"; then
+  if grep -q '"barryvdh/laravel-ide-helper"' 'composer.json'; then
     cd $(pwd)
-    docker-compose exec laravel bash -c "php artisan ide-helper:generate && php artisan ide-helper:meta && php artisan ide-helper:models --write-mixin"
+    docker-compose exec laravel bash -c 'php artisan ide-helper:generate && php artisan ide-helper:meta && php artisan ide-helper:models --write-mixin'
   fi
 
-  if grep -q "\"friendsofphp/php-cs-fixer\"" "composer.json"; then
+  if grep -q '"friendsofphp/php-cs-fixer"' 'composer.json'; then
     cd $(pwd)
     docker-compose exec laravel ./vendor/bin/php-cs-fixer fix
   fi
-elif [[ "$1" == "--help" ]]; then
+elif [[ "$1" == '--help' ]]; then
   echo 'See: https://larasurf.com/docs'
 else
   cd $(pwd)
