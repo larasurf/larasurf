7aa9bf13b0195649e498d556ed6b5a665bf789bc
Brice Hartmann <brice@bricehartmann.com>
2021-09-19 00:22:48 -0500

    config command tests

--- src/Commands/Config.php
+++ src/Commands/Config.php
@@ -11,42 +11,68 @@ class Config extends Command
     use HasSubCommands;
     use InteractsWithLaraSurfConfig;
 
+    /**
+     * The available subcommands to run.
+     */
     const COMMAND_GET = 'get';
     const COMMAND_SET = 'set';
 
+    /**
+     * @var string
+     */
     protected $signature = 'larasurf:config
                             {subcommand : The subcommand to run: \'get\' or \'set\'}
                             {key : The config key, supports dot notation}
                             {value? : The config value, required with \'set\'}';
 
+    /**
+     * @var string
+     */
     protected $description = 'Read and update the LaraSurf configuration file';
 
+    /**
+     * A mapping of subcommands => method name to call.
+     *
+     * @var string[]
+     */
     protected array $commands = [
         self::COMMAND_GET => 'handleGet',
         self::COMMAND_SET => 'handleSet',
     ];
 
+    /**
+     * Get a value from the LaraSurf configuration file using dot notation.
+     *
+     * @return int
+     */
     protected function handleGet()
     {
         $key = $this->argument('key');
 
         $value = static::larasurfConfig()->get($key);
 
-        if ($value !== null) {
-            if (is_bool($value)) {
-                $value = $value ? 'true' : 'false';
-            }
-
-            $this->line($value);
-        } else {
+        if ($value === null) {
             $this->error("Key '$key' not found in '" . static::laraSurfConfigFilePath() . "'");
 
             return 1;
         }
 
+        if (is_bool($value)) {
+            $value = $value ? 'true' : 'false';
+        }
+
+        $this->line($value);
+
         return 0;
     }
 
+    /**
+     * Set a value in the LaraSurf configuration file using dot notation.
+     *
+     * @return int
+     * @throws \LaraSurf\LaraSurf\Exceptions\Config\InvalidConfigKeyException
+     * @throws \LaraSurf\LaraSurf\Exceptions\Config\InvalidConfigValueException
+     */
     protected function handleSet()
     {
         $value = $this->argument('value');
---
+++ tests/Feature/Commands/ConfigTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace LaraSurf\LaraSurf\Tests\Feature\Commands;
+
+use Illuminate\Support\Str;
+use LaraSurf\LaraSurf\Config;
+use LaraSurf\LaraSurf\Tests\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testGet()
+    {
+        $this->createValidLaraSurfConfig('local-stage-production');
+
+        $this->artisan('larasurf:config get project-name')
+            ->expectsOutput($this->project_name)
+            ->assertExitCode(0);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testGetDoesntExist()
+    {
+        $this->createValidLaraSurfConfig('local-stage-production');
+
+        $key = $this->faker->word;
+
+        $this->artisan('larasurf:config get ' . $key)
+            ->expectsOutput("Key '$key' not found in 'larasurf.json'")
+            ->assertExitCode(1);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testGetDotNotation()
+    {
+        $this->createValidLaraSurfConfig('local-stage-production');
+
+        $this->artisan('larasurf:config get environments.production.aws-region')
+            ->expectsOutput($this->aws_region)
+            ->assertExitCode(0);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testSet()
+    {
+        $this->createValidLaraSurfConfig('local-stage-production');
+
+        $value = strtolower(Str::random());
+
+        $this->artisan('larasurf:config set project-name ' . $value)
+            ->expectsOutput("File 'larasurf.json' updated successfully")
+            ->assertExitCode(0);
+
+        $config = new Config();
+        $this->assertEquals($value, $config->get('project-name'));
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testSetDotNotation()
+    {
+        $this->createValidLaraSurfConfig('local-stage-production');
+
+        $value = 'us-east-1';
+
+        $this->artisan('larasurf:config set environments.production.aws-region ' . $value)
+            ->expectsOutput("File 'larasurf.json' updated successfully")
+            ->assertExitCode(0);
+
+        $config = new Config();
+        $this->assertEquals($value, $config->get('environments.production.aws-region'));
+    }
+}
